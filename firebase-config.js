92cd68f7126753bd306a934ed109208db0b76474
Giang <114056628+Giangha20@users.noreply.github.com>
2026-09-20 08:09:56 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
-// firebase-config.js
-// DÁN Firebase Web App config của bạn vào đây.
-// Firebase Console -> Project settings -> Your apps -> Web app
-const firebaseConfig = {
-  apiKey: "AIzaSyAlLNC7D1YKHHKqYnAMwsJjtn8AdouNXf4",
-  authDomain: "chines-31df6.firebaseapp.com",
-  projectId: "chines-31df6",
-  storageBucket: "chines-31df6.firebasestorage.app",
-  messagingSenderId: "8282005311",
-  appId: "1:8282005311:web:fa3e1d80fead34245a7d8a"
+/* Firebase Web App config */
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAlLNC7D1YKHHKqYnAMwsJjtn8AdfeF4",
+    authDomain: "chines-31df6.firebaseapp.com",
+    projectId: "chines-31df6",
+    storageBucket: "chines-31df6.firebasestorage.app",
+    messagingSenderId: "8282005311",
+    appId: "1:8282005311:web:fa3e1d80fead34245a7d8a"
 };
