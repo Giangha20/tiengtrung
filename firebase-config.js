d4fe6b393988d48412c516a956c0ee0a49c26996
Giang <114056628+Giangha20@users.noreply.github.com>
2026-09-20 08:14:20 +0700

    Add Firebase configuration and initialization
    Added Firebase initialization code and configuration.

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,17 @@
-// firebase-config.js // DÁN Firebase Web App config của bạn vào đây. // Firebase Console -> Project settings -> Your apps -> Web app const firebaseConfig = { apiKey: "AIzaSyAlLNC7D1YKHHKqYnAMwsJjtn8AdouNXf4", authDomain: "chines-31df6.firebaseapp.com", projectId: "chines-31df6", storageBucket: "chines-31df6.firebasestorage.app", messagingSenderId: "8282005311", appId: "1:8282005311:web:fa3e1d80fead34245a7d8a" };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlLNC7D1YKHHKqYnAMwsJjtn8AdouNXf4",
+  authDomain: "chines-31df6.firebaseapp.com",
+  projectId: "chines-31df6",
+  storageBucket: "chines-31df6.firebasestorage.app",
+  messagingSenderId: "8282005311",
+  appId: "1:8282005311:web:fa3e1d80fead34245a7d8a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
